eeff755740faa5fde06ca7794beebd7275cf9b9d
KhushiN02 <aakritine1212@gmail.com>
2024-12-09 20:02:16 -0800

    first commit

---
+++ classwork/custom.js
@@ -0,0 +1,11 @@
+let name="aakriti"
+let roll_number="5"
+let email="aakritine12@gmail.com"
+let address="kathmandu"
+let phone="9869910278"
+document.getElementById("name").innerHTML=name
+document.getElementById("roll_number").innerHTML=roll_number
+document.getElementById("email").innerHTML=email
+document.getElementById("address").innerHTML=address
+document.getElementById("phone").innerHTML=phone
+console.log
